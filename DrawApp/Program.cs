f63b8ad85a7cf990d1dea3ea2194db1192e87b51
Taavi Hautoniemi <taavihautoniemi@hotmail.com>
2016-01-26 10:09:54 +0200

    Initial commit.

---
+++ DrawApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DrawingObject do = new DrawingObject();
+
+            Circle circle = new Circle();
+            circle.Name = "My circle";
+            circle.Draw();
+
+            Square square = new Square();
+            square.Name = "My square";
+            square.Draw();
+
+            List<DrawingObject> drawingObjects = new List<DrawingObject>();
+            // add new objects t ocollection
+            drawingObjects.Add(new Circle());
+            drawingObjects.Add(new Square());
+            drawingObjects.Add(new Circle());
+            drawingObjects.Add(new Square());
+            drawingObjects.Add(new Circle());
+            drawingObjects.Add(new Square());
+
+            // loop collection and draw
+            Console.WriteLine("Drawing objects in a list.");
+            foreach(DrawingObject drawingObject in drawingObjects)
+            {
+                drawingObject.Draw();
+            }
+
+            Console.WriteLine("Press enter to continue...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ T1/Tyres.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1
+{
+    class Tyres : Vehicle
+    {
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string Size { get; set; }
+    }
+}
